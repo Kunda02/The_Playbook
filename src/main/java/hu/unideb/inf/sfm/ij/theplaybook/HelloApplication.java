f883c53dd784b3f93f139b5a58d0465ca9d83f49
Károly Róbert <karolyrobert01@gmail.com>
2023-03-31 22:57:59 +0200

    mükszik

--- src/main/java/hu/unideb/inf/sfm/ij/theplaybook/HelloApplication.java
+++ src/main/java/hu/unideb/inf/sfm/ij/theplaybook/HelloApplication.java
@@ -20,8 +20,8 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
         //commit
-        System.out.println("Hello2");
-        System.out.println("Hello2");
+        System.out.println("Működik a commit");
+        System.out.println("Ha nem megy szóljatok");
 
     }
 }
