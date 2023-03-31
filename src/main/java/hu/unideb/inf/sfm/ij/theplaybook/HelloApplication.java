4c4f987fcdd457ecf205bbc01634487a9adabb70
Károly Róbert <karolyrobert01@gmail.com>
2023-03-31 22:52:23 +0200

    try3

--- src/main/java/hu/unideb/inf/sfm/ij/theplaybook/HelloApplication.java
+++ src/main/java/hu/unideb/inf/sfm/ij/theplaybook/HelloApplication.java
@@ -20,5 +20,7 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
         //commit
+        System.out.println("Hello");
+        
     }
 }
