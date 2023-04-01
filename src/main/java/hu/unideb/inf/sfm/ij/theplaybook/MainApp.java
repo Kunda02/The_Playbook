4daa6ef3bef0308c91b38b6d1ef5c95aca060ebc
Kunda02 <kun.daniel02@gmail.com>
2023-04-01 21:39:21 +0200

    fetch debug 33

--- src/main/java/hu/unideb/inf/sfm/ij/theplaybook/MainApp.java
+++ src/main/java/hu/unideb/inf/sfm/ij/theplaybook/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/Scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello1!");
+        stage.setTitle("Hello1!33");
         stage.setScene(scene);
         stage.show();
     }
