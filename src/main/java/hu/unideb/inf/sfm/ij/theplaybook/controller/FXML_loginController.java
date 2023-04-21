2ab135e373828c6d18deeeab14b3caebe75d7fa5
Károly Róbert <karolyrobert01@gmail.com>
2023-04-21 22:00:59 +0200

    Start login 3 (login to pages)

--- src/main/java/hu/unideb/inf/sfm/ij/theplaybook/controller/FXML_loginController.java
+++ src/main/java/hu/unideb/inf/sfm/ij/theplaybook/controller/FXML_loginController.java
@@ -1,12 +1,17 @@
 package hu.unideb.inf.sfm.ij.theplaybook.controller;
-
-import hu.unideb.inf.sfm.ij.theplaybook.MainApp;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class FXML_loginController {
 
@@ -21,4 +26,27 @@ public class FXML_loginController {
     public PasswordField loginPassword;
     @FXML
     public Label loginStatus;
+
+    @FXML
+    void loginHandler(ActionEvent event) throws IOException {
+        loginBtn.setDisable(true);
+        loginStatus.setText("Bejelentkezés...");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/FXML_main.fxml"));
+        Parent mainParent = loader.load();
+
+        FXML_mainController mainController = loader.getController();
+        //mainController.setDao(pDAO);
+
+        Scene mainScene = new Scene(mainParent);
+        Stage stage = new Stage();
+        stage.setTitle("Oldal");
+        stage.setScene(mainScene);
+        stage.show();
+
+        Stage original = (Stage)loginName.getScene().getWindow();
+        original.close();
+
+
+    }
 }
--- src/main/java/hu/unideb/inf/sfm/ij/theplaybook/controller/FXML_mainController.java
+++ src/main/java/hu/unideb/inf/sfm/ij/theplaybook/controller/FXML_mainController.java
@@ -1,4 +1,15 @@
 package hu.unideb.inf.sfm.ij.theplaybook.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class FXML_mainController {
+    @FXML
+    public Button exit;
+    @FXML
+    public Button delete;
+    @FXML
+    public Button add;
+    @FXML
+    public Button back;
 }
