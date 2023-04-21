7084602f89eb2bc89fb5b2c11490dd5249dbf89d
Károly Róbert <karolyrobert01@gmail.com>
2023-04-21 22:18:15 +0200

    Start login 6 (exitBtn)

--- src/main/java/hu/unideb/inf/sfm/ij/theplaybook/controller/FXML_mainController.java
+++ src/main/java/hu/unideb/inf/sfm/ij/theplaybook/controller/FXML_mainController.java
@@ -2,7 +2,13 @@ package hu.unideb.inf.sfm.ij.theplaybook.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class FXML_mainController {
     @FXML
@@ -14,6 +20,32 @@ public class FXML_mainController {
     @FXML
     public Button back;
 
-    public void exitHandler(ActionEvent actionEvent) {
+    public void exitHandler(ActionEvent actionEvent) throws IOException {
+        exit.setDisable(true);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/FXML_login.fxml"));
+        Parent loginParent = loader.load();
+
+        FXML_loginController loginController = loader.getController();
+        //mainController.setDao(pDAO);
+
+        Scene loginScene = new Scene(loginParent);
+        Stage stage = new Stage();
+        stage.setTitle("Login");
+        stage.setScene(loginScene);
+        stage.show();
+        //commit
+        //Stage original = (Stage).getScene().getWindow();
+        //original.close();
+
+    }
+
+    public void backHandler(ActionEvent actionEvent) {
+    }
+
+    public void addHandler(ActionEvent actionEvent) {
+    }
+
+    public void deleteHandler(ActionEvent actionEvent) {
     }
 }
