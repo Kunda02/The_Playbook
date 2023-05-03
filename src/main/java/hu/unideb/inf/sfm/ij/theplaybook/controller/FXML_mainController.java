17ccbced49aada98754ca7e0df9a74145d6204d8
Károly Róbert <karolyrobert01@gmail.com>
2023-05-03 19:22:25 +0200

    Commit debug 2

--- src/main/java/hu/unideb/inf/sfm/ij/theplaybook/controller/FXML_mainController.java
+++ src/main/java/hu/unideb/inf/sfm/ij/theplaybook/controller/FXML_mainController.java
@@ -81,6 +81,7 @@ public class FXML_mainController implements Initializable {
 
     public void backHandler(ActionEvent actionEvent) {
         //
+        ////
     }
 
     public void addHandler(ActionEvent actionEvent) {
