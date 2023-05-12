db357ade2921d470a1903b92f48a5518f4efc65a
Kunda02 <kun.daniel02@gmail.com>
2023-05-12 21:06:56 +0200

    Dani, finalyzing #2

--- src/main/java/hu/unideb/inf/sfm/ij/theplaybook/MainApp.java
+++ src/main/java/hu/unideb/inf/sfm/ij/theplaybook/MainApp.java
@@ -24,6 +24,7 @@ public class MainApp extends Application {
         stage.setResizable(false);
         stage.show();
     }
+    //Commit with comment #1
 
     public static void main(String[] args) throws SQLException {
         //startDatabase();
